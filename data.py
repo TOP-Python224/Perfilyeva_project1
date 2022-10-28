e6058dca2a5b69a4360eae3d4a07e6e8d04e05db
Gennadiy <renardes@gmail.com>
2022-10-28 09:23:05 +0500

    правки удобства чтения кода

--- data.py
+++ data.py
@@ -1,16 +1,19 @@
 """Дополнительный модуль: глобальные переменные и константы."""
 
+# импорт из стандартной библиотеки
 from pathlib import Path
 from sys import path
 
-# глобальные константы
-APP_TITLE = "КРЕСТИКИ-НОЛИКИ"
-PROMPT = ' > '
+
+# глобальные переменные данных
+STATS = {}
+SAVES = {}
 
 script_dir = Path(path[0])
 players_ini_path = script_dir / 'players.ini'
 saves_ini_path = script_dir / 'saves.ini'
 
-# глобальные переменные данных
-STATS = {}
-SAVES = {}
+
+# глобальные константы
+APP_TITLE = "КРЕСТИКИ-НОЛИКИ"
+PROMPT = ' > '
--- functions.py
+++ functions.py
@@ -1,10 +1,14 @@
 """Дополнительный модуль: вспомогательные функции."""
 
+# импорт из стандартной библиотеки
 import configparser
+
+# импорт дополнительных модулей проекта
 import data
 
 
 def read_ini():
+    # players.ini -> data.STATS
     players = configparser.ConfigParser()
     players.read(data.players_ini_path)
     for name in players.sections():
@@ -12,7 +16,7 @@ def read_ini():
         for option in players[name]:
             dict_option[option] = players[name][option]
         data.STATS[name] = dict_option
-
+    # saves.ini -> data.SAVES
     saves = configparser.ConfigParser()
     saves.read(data.saves_ini_path)
     for name_players in saves.sections():
@@ -23,12 +27,13 @@ def read_ini():
 
 
 def write_ini():
+    # data.STATS -> players.ini
     players = configparser.ConfigParser()
     for player in data.STATS:
         players[player] = data.STATS[player]
     with open(data.players_ini_path, 'w', encoding='utf-8') as stats_files:
         players.write(stats_files)
-
+    # data.SAVES -> saves.ini
     saves = configparser.ConfigParser()
     for name_players in data.SAVES:
         saves[name_players] = data.SAVES[name_players]
@@ -36,11 +41,8 @@ def write_ini():
         saves.write(saves_files)
 
 
+# тесты
 if __name__ == '__main__':
     write_ini()
 
-
-
 read_ini()
-
-
