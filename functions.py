32f811b82e0458b7c0b3641fdea1808e4ed0fe4d
Gennadiy <renardes@gmail.com>
2022-10-28 09:27:15 +0500

    работа с импортами

--- functions.py
+++ functions.py
@@ -1,15 +1,17 @@
 """Дополнительный модуль: вспомогательные функции."""
 
+# ИСПОЛЬЗОВАТЬ: для импорта из стандартной библиотеки мы стараемся использовать преимущественно инструкцию from...import... — это связано с тем, что модули стандартной библиотеки объёмные, но целиком нужны редко
 # импорт из стандартной библиотеки
-import configparser
+from configparser import ConfigParser
 
+# ИСПОЛЬЗОВАТЬ: наоборот, импорт модулей текущего проекта мы чаще осуществляем с помощью инструкции import, для предупреждения закольцованности импортов
 # импорт дополнительных модулей проекта
 import data
 
 
 def read_ini():
     # players.ini -> data.STATS
-    players = configparser.ConfigParser()
+    players = ConfigParser()
     players.read(data.players_ini_path)
     for name in players.sections():
         dict_option = {}
@@ -17,7 +19,7 @@ def read_ini():
             dict_option[option] = players[name][option]
         data.STATS[name] = dict_option
     # saves.ini -> data.SAVES
-    saves = configparser.ConfigParser()
+    saves = ConfigParser()
     saves.read(data.saves_ini_path)
     for name_players in saves.sections():
         dict_option = {}
@@ -28,13 +30,13 @@ def read_ini():
 
 def write_ini():
     # data.STATS -> players.ini
-    players = configparser.ConfigParser()
+    players = ConfigParser()
     for player in data.STATS:
         players[player] = data.STATS[player]
     with open(data.players_ini_path, 'w', encoding='utf-8') as stats_files:
         players.write(stats_files)
     # data.SAVES -> saves.ini
-    saves = configparser.ConfigParser()
+    saves = ConfigParser()
     for name_players in data.SAVES:
         saves[name_players] = data.SAVES[name_players]
     with open(data.saves_ini_path, 'w', encoding='utf-8') as saves_files:
